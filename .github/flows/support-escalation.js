8e0f678d4c459129eae1e422a392381cd3f520ef
Yoann Chaudet <yoannchaudet@github.com>
2022-09-23 15:41:58 -0700

    support

--- .github/flows/support-escalation.js
+++ .github/flows/support-escalation.js
@@ -1,15 +1,17 @@
 const config = {
-  name: 'Support escalation',
+  name: 'Support escalation'
 }
 
-console.log('Hello there')
+console.log('Starting flow')
 
-onInit().do(async () => {
+onInit('main').do(async () => {
   // Run the first time (or when the workflow is edited)
+  console.log('On init')
 })
 
-onSchedule('').do(async () =>{
+onSchedule('*/10 * * * *').do(async () => {
   // Run on a schedule
+  console.log('On schedule (every 10 min)')
 })
 
 // Add to project if needed
@@ -17,9 +19,10 @@ onIssue()
   .type('opened')
   .withLabel('support-escalation')
   .do(async issue => {
-    if (!getProjectColumn(project, issue)) {
-      setProjectColumn(project, issue, triageColumn)
-    }
+    console.log('On issue (opened) with label support-escalation')
+    // if (!getProjectColumn(project, issue)) {
+    //   setProjectColumn(project, issue, triageColumn)
+    // }
   })
 
 // Awaiting customer response
@@ -27,7 +30,8 @@ onIssue()
   .type('labeled')
   .withLabel('awaiting-customer')
   .do(async issue => {
-    setProjectColumn(project, issue, blockedColumn)
+    // setProjectColumn(project, issue, blockedColumn)
+    console.log('On issue (labeled) with label awaiting-customer')
   })
 
 // Back to triage
@@ -35,5 +39,8 @@ onIssue()
   .type('unlabeled')
   .withLabel('awaiting-customer')
   .do(async issue => {
-    setProjectColumn(project, issue, triageColumn)
+    // setProjectColumn(project, issue, triageColumn)
+    console.log('On issue (unlabeled) with label awaiting-customer')
   })
+
+console.log('Ending flow')
