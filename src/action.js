5e299704ba1a6a5284cd9dfd65d5f1a7a70a717d
Yoann Chaudet <yoannchaudet@github.com>
2022-09-23 15:20:30 -0700

    fix wording

--- src/action.js
+++ src/action.js
@@ -2,7 +2,7 @@ const core = require('@actions/core')
 const { Runner } = require('./runner')
 
 // Get inputs
-const flowPath = core.getInput('flow-path')
+const flowPath = core.getInput('path')
 
 // Run the flow
 new Runner(flowPath).run()
